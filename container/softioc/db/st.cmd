b36d5efea855042c1180c436b6452d4d9928d657
Ryan Slominski <Ryan.Slominski@gmail.com>
2025-08-21 11:57:55 -0400

    test records

--- container/softioc/db/st.cmd
+++ container/softioc/db/st.cmd
@@ -1,7 +1,13 @@
 dbLoadRecords("/db/softioc.db")
-dbLoadRecords("/db/macros.db", "HALL=A")
-dbLoadRecords("/db/macros.db", "HALL=C")
-dbLoadRecords("/db/macros.db", "HALL=D")
+dbLoadRecords("/db/hallmacros.db", "HALL=A")
+dbLoadRecords("/db/hallmacros.db", "HALL=C")
+dbLoadRecords("/db/hallmacros.db", "HALL=D")
+dbLoadRecords("/db/icamacros.db", "IC=IIC1H00")
+dbLoadRecords("/db/icamacros.db", "IC=IIC1H05")
+dbLoadRecords("/db/icamacros.db", "IC=IIC1H04A")
+dbLoadRecords("/db/icamacros.db", "IC=IIC1P03")
+dbLoadRecords("/db/icamacros.db", "IC=IICD107")
+dbLoadRecords("/db/icamacros.db", "IC=IICD107A")
 
 iocInit
 iocRun
